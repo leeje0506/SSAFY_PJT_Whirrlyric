6b395c0b563f98819d0c31841cda14dc9d291aa2
sujin <ansujin0204@naver.com>
2024-04-29 13:13:08 +0900

    Feat: 라우터 경로 설정

---
+++ FrontEnd/fe-project/src/pages/PlaySong.tsx
@@ -0,0 +1,5 @@
+export default function PlaySong() {
+  return (
+    <div>PlaySong</div>
+  )
+}
--- FrontEnd/fe-project/src/routes/routes.tsx
+++ FrontEnd/fe-project/src/routes/routes.tsx
@@ -1,7 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../pages/RootLayout";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MyPage from "../pages/MyPage";
+import MakeSong from "../pages/MakeSong";
+import PlaySong from "../pages/PlaySong";
 
 const routes = createBrowserRouter([
-
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "main",
+        element: <Main />,
+      },
+      {
+        path: "make-song",
+        element: <MakeSong />,
+      },
+      {
+        path: "play-song",
+        element: <PlaySong />,
+      },
+      {
+        path: "mypage",
+        element: <MyPage />,
+      },
+    ],
+  },
 ]);
 
 export default routes;
