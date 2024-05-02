03e3ad81f683278febe690f1684ddc620ff78bb9
sujin <ansujin0204@naver.com>
2024-05-02 14:07:36 +0900

    Feat: 버튼 컴포넌트 추가 및 반영

--- FrontEnd/fe-project/src/components/common/Button.tsx
+++ FrontEnd/fe-project/src/components/common/Button.tsx
@@ -1,7 +1,16 @@
-export default function Button() {
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  addClass?: string;
+}
+
+export default function Button(buttonProps: ButtonProps) {
   return (
-    <button>
-        
+    <button
+      className={`bg-black text-white rounded-3xl w-80 h-12 ${buttonProps.addClass}`}
+      onClick={buttonProps.onClick}
+    >
+      {buttonProps.label}
     </button>
-  )
+  );
 }
