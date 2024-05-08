9f9e531b47feac2aa8aa706c694a013b5442a6c5
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-05-08 13:16:59 +0900

    Feat : Repository 수정

--- Backend/jayul/src/main/java/com/example/song/repository/MainsongRepository.java
+++ Backend/jayul/src/main/java/com/example/song/repository/MainsongRepository.java
@@ -3,6 +3,6 @@ package com.example.song.repository;
 import com.example.song.domain.Mainsong;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MainsongRepository extends JpaRepository<Mainsong, Long> {
+public interface MainsongRepository extends JpaRepository<Mainsong, Integer> {
 
 }
--- Backend/jayul/src/main/java/com/example/song/repository/SongRepository.java
+++ Backend/jayul/src/main/java/com/example/song/repository/SongRepository.java
@@ -3,7 +3,7 @@ package com.example.song.repository;
 import com.example.song.domain.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SongRepository extends JpaRepository<Song, Long> {
+public interface SongRepository extends JpaRepository<Song, Integer> {
     // 필요한 커스텀 메소드 추가 가능
 }
 
