53692c52f53cb4e53878aa308569526db4df9576
main-character-coding <yerimin1999@gmail.com>
2024-05-10 16:05:10 +0900

    Feat : 닉네임 수정 관련 DTO

---
+++ Backend/jayul/src/main/java/com/example/member/dto/res/MemberEditDto.java
@@ -0,0 +1,14 @@
+package com.example.member.dto.res;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record MemberEditDto(
+    @Size(min = 2, max = 15, message = "잘못된 요청 형식입니다. nickname은 2글자 이상 15글자 이하입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣\\\\-\\\\_]*$", message = "잘못된 요청 형식입니다. 특수문자(~!@#$%^&*())와 공백을 제외해야 합니다.")
+    String nickname
+) {
+
+}
