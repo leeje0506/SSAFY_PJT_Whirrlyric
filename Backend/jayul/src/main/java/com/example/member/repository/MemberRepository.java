e26514958136f2982ceb6dc3b1b6c3b4e6ce2b4f
main-character-coding <yerimin1999@gmail.com>
2024-05-10 15:16:20 +0900

    Feat: 닉네임 찾는 기능, OauthId 찾는 기능 repo에 추가

--- Backend/jayul/src/main/java/com/example/member/repository/MemberRepository.java
+++ Backend/jayul/src/main/java/com/example/member/repository/MemberRepository.java
@@ -1,8 +1,13 @@
 package com.example.member.repository;
 
 import com.example.member.domain.Member;
+import com.example.member.domain.OauthId;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByOauthId(OauthId oauthId);
+
+    Optional<Member> findByNickname(String nickname);
 }
