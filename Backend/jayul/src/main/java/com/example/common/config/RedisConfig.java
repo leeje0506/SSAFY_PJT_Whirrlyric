e134da36873801f4ee54d25bd35d46f9b55de2dd
main-character-coding <yerimin1999@gmail.com>
2024-05-10 15:57:27 +0900

    fix : redis TransactionManager 수정

--- Backend/jayul/src/main/java/com/example/common/config/RedisConfig.java
+++ Backend/jayul/src/main/java/com/example/common/config/RedisConfig.java
@@ -1,19 +1,27 @@
 package com.example.common.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@ConfigurationProperties(prefix = "spring.data.redis")
 @EnableTransactionManagement
+@Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
     private String host;
+
+    @Value("${spring.data.redis.password}")
     private String password;
+
+    @Value("${spring.data.redis.port}")
     private int port;
 
     private LettuceConnectionFactory createLettuceConnectionFactory() {
@@ -37,5 +45,10 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
     }
 }
