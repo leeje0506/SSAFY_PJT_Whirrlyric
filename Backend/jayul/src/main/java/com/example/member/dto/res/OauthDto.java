a8dae17b1380d086c3af7fdf90eca25e299d3858
main-character-coding <yerimin1999@gmail.com>
2024-05-10 15:45:11 +0900

    Refactor : 구글 포매터를 통해 리팩토링을 함

---
+++ Backend/jayul/src/main/java/com/example/member/dto/res/OauthDto.java
@@ -0,0 +1,12 @@
+package com.example.member.dto.res;
+
+import com.example.member.domain.Member;
+
+import lombok.Builder;
+
+@Builder
+public record OauthDto(
+	Member member,
+	String accessToken
+) {
+}
