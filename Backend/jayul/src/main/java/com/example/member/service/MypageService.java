59e073b70bd2f91daf07ba5f48e7c5f29021936d
main-character-coding <yerimin1999@gmail.com>
2024-05-10 16:06:19 +0900

    Fix : MemberCustomRepository memberCustomRepository로 임의로 수정 - 회원 가입 탈퇴 - 닉네임 수정

--- Backend/jayul/src/main/java/com/example/member/service/MypageService.java
+++ Backend/jayul/src/main/java/com/example/member/service/MypageService.java
@@ -1,13 +1,21 @@
 package com.example.member.service;
 
+import com.example.common.exception.BaseException;
+import com.example.common.exception.ErrorCode;
 import com.example.member.domain.Member;
+import com.example.member.domain.type.OauthServerType;
+import com.example.member.dto.res.MemberEditDto;
 import com.example.member.dto.res.MypageProfileResponse;
 import com.example.member.repository.MemberCustomRepository;
+import com.example.member.repository.MemberRepository;
 import com.example.song.domain.Mainsong;
 import com.example.song.domain.Song;
 import com.example.song.dto.res.SongResponse;
 import com.example.song.repository.*;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,13 +24,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class MypageService {
-    private final MemberCustomRepository memberRepository;
+
+    private final MemberCustomRepository memberCustomRepository;
     private final MainsongCustomRepository mainSongCustomRepository;
     private final SongCustomRepository songRepository;
     private final MainsongRepository mainsongRepository;
+    private final MemberRepository memberRepository;
+
+    private final OauthService oauthService;
+    private final TokenService tokenService;
 
     public MypageProfileResponse getMemberProfile(int memberId) {
-        Member member = memberRepository.findMemberById(memberId);
+        Member member = memberCustomRepository.findMemberById(memberId);
 
         Mainsong mainSong = mainSongCustomRepository.findMainsongByMemberId(memberId);
 
@@ -52,13 +65,13 @@ public class MypageService {
     public Integer setMainSong(int songId, String nickname) {
         Song song = songRepository.findSongBySongId(songId);
 
-        Member member = memberRepository.findMemberByNickname(nickname);
+        Member member = memberCustomRepository.findMemberByNickname(nickname);
 
         Mainsong existedMainsong = mainSongCustomRepository.findExistedMainsong(member);
 
         Mainsong savedSong;
 
-        if(existedMainsong == null){    // 대표곡을 처음 설정할 때
+        if (existedMainsong == null) {    // 대표곡을 처음 설정할 때
             Mainsong mainsong = Mainsong.builder()
                 .song(song)
                 .member(member)
@@ -70,5 +83,29 @@ public class MypageService {
         }
 
         return savedSong.getId();
+    }
+
+    public MemberEditDto updateMember(Member member, MemberEditDto request) {
+        Optional<Member> existingMember = memberRepository.findByNickname(request.nickname());
+        if (existingMember.isPresent()) {
+            throw new BaseException(ErrorCode.MEMBER_NICKNAME_DUPLICATED);
+        }
+
+        member.setNickname(request.nickname());
+        memberRepository.save(member);
+
+        return MemberEditDto.builder().nickname(request.nickname()).build();
+    }
+
+    @Operation(
+        summary = "회원가입 탈퇴"
+    )
+    public void deleteMember(HttpServletResponse response, OauthServerType oauthServerType,
+        Member member) {
+        oauthService.logout(response, oauthServerType,
+            member.getOauthId().getOauthServerId());   //카카오톡 로그아웃
+        tokenService.deleteHeader(response);
+        member.setIsDeleted(true);
+        memberRepository.save(member);
     }
 }
