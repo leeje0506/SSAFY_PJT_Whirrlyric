303b26f9bcf1f5751c7b777098665b565603c047
main-character-coding <yerimin1999@gmail.com>
2024-05-10 15:42:34 +0900

    Refactor : 구글 포매터를 통해 리팩토링을 함

--- Backend/jayul/src/main/java/com/example/common/exception/ErrorResponse.java
+++ Backend/jayul/src/main/java/com/example/common/exception/ErrorResponse.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class ErrorResponse {
+
     private Integer errorCode;
     private String errorMsg;
 }
--- Backend/jayul/src/main/java/com/example/common/exception/GlobalExceptionHandler.java
+++ Backend/jayul/src/main/java/com/example/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.common.exception;
 
 import static com.example.common.exception.ErrorCode.METHOD_ARGUMENT_NOT_VALID;
 
+import java.util.Objects;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
         return ResponseEntity.status(ex.getErrorCode().getErrorCode())
-            .body(new ErrorResponse(ex.getErrorCode().getErrorCode(), ex.getErrorCode().getErrorMsg()));
+            .body(new ErrorResponse(ex.getErrorCode().getErrorCode(),
+                ex.getErrorCode().getErrorMsg()));
     }
 
     // 유효성 검사 base 예외 처리에 맞게 custom
@@ -21,6 +24,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
         MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(new ErrorResponse(METHOD_ARGUMENT_NOT_VALID.getErrorCode(), e.getFieldError().getDefaultMessage()));
+            .body(new ErrorResponse(METHOD_ARGUMENT_NOT_VALID.getErrorCode(),
+                Objects.requireNonNull(e.getFieldError()).getDefaultMessage()));
     }
 }
