9fb77080a2c5ce3a13fe4e8b5a12f98df3f47987
유호영 <yhu2121@naver.com>
2024-05-13 13:48:05 +0900

    Feat: Security 관련 설정 변경
    - CorsConfig 빈 분리
    - swagger 경로 추가
    - securityConfig에 cors 설정 추가

--- Backend/jayul/src/main/java/com/example/common/config/SecurityConfig.java
+++ Backend/jayul/src/main/java/com/example/common/config/SecurityConfig.java
@@ -20,10 +20,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig implements WebMvcConfigurer {
-
+    private final CorsConfig corsConfig;
     private final TokenService tokenService;
+    private static final String[] PERMIT_URL_ARRAY = {
+        // swagger v3
+        "/v3/api-docs/**",
+        "/swagger-ui/**",
+
+        // oauth
+        "/oauth/kakao",
+        "/oauth/redirected/kakao",
+        "/oauth/login/**",
+        "/members/reissue"
+    };
+
 
     //WebMVCConfigurer 인터페이스의 메소드 구현
     @Override
@@ -40,36 +52,15 @@ public class SecurityConfig implements WebMvcConfigurer {
                 // OPTIONS 메소드에 대한 요청은 인증 절차 없이 허용
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 // 특정 경로에 대한 접근 허용
-                .requestMatchers(
-                    "/oauth/kakao",
-                    "/oauth/redirected/kakao",
-                    "/oauth/login/**",
-                    "/members/reissue",
-                    "/swagger-ui",
-                    "/swagger-ui/**"
-                ).permitAll()
+                .requestMatchers(PERMIT_URL_ARRAY).permitAll()
                 .anyRequest().authenticated()
-            ).addFilterBefore(new JwtAuthenticationFilter(tokenService),
+            )
+            .cors(cors-> cors.configurationSource(corsConfig.corsConfigurationSource()))
+            .addFilterBefore(new JwtAuthenticationFilter(tokenService),
                 UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling(
                 e -> e.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
         return http.build();
-    }
-
-    //CORS 구성을 정의
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(
-            Arrays.asList("https://whirrlyric.n-e.kr", "http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowCredentials(true); // 자격증명 허용
-        configuration.setExposedHeaders(
-            Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
