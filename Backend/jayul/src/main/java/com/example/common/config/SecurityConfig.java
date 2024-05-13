4c046df8731aaeea6b9edaeea4f17a7db2ab9ca4
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-05-13 13:01:17 +0900

    Feat : WebConfig 삭제 및 SecurityConfig 수정

--- Backend/jayul/src/main/java/com/example/common/config/SecurityConfig.java
+++ Backend/jayul/src/main/java/com/example/common/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.common.config;
 
 import com.example.member.service.TokenService;
+import com.example.member.util.OauthServerTypeConverter;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,19 +15,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     private final TokenService tokenService;
+
+    //WebMVCConfigurer 인터페이스의 메소드 구현
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new OauthServerTypeConverter());
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable)
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .formLogin(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
@@ -41,25 +49,24 @@ public class SecurityConfig {
                     "/swagger-ui/**"
                 ).permitAll()
                 .anyRequest().authenticated()
-            );
-
-        http.addFilterBefore(new JwtAuthenticationFilter(tokenService),
-            UsernamePasswordAuthenticationFilter.class);
-        http.exceptionHandling(
-            e -> e.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+            ).addFilterBefore(new JwtAuthenticationFilter(tokenService),
+                UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(
+                e -> e.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
         return http.build();
     }
 
+    //CORS 구성을 정의
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(
+            Arrays.asList("https://whirrlyric.n-e.kr", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true); // 자격증명 허용
-        configuration.setExposedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-
+        configuration.setExposedHeaders(
+            Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
