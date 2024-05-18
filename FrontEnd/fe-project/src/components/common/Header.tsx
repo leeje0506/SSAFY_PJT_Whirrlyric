cfb7a19d4e1576de5e4d61ad9f3ed7469c0f79e6
sujin <ansujin0204@naver.com>
2024-05-18 21:53:28 +0900

    Feat: 현재 시간 반영

--- FrontEnd/fe-project/src/components/common/Header.tsx
+++ FrontEnd/fe-project/src/components/common/Header.tsx
@@ -4,10 +4,27 @@ import batteryIcon from "../../assets/icons/rootlayout/batteryIcon";
 import chartBarIcon from "../../assets/icons/rootlayout/chartBarIcon";
 import speakerIcon from "../../assets/icons/rootlayout/speakerIcon";
 import wifiIcon from "../../assets/icons/rootlayout/wifiIcon";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      setCurrentTime(`${hours} : ${minutes}`);
+    };
+
+    updateTime();
+    const intervalId = setInterval(updateTime, 1000); 
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   const getHeaderName = (pathname: string) => {
     if (pathname.startsWith("/play-song")) {
@@ -28,7 +45,7 @@ export default function Header() {
   return (
       <div className="flex items-center justify-center w-full fixed z-50">
         <div className="flex items-start fixed w-[430px] h-12 bg-gray-700">
-          <span className="pt-6 text-white ml-3">12 : 14</span>
+          <span className="pt-6 text-white ml-3">{currentTime}</span>
           <div className="flex-grow"></div>
           <span className="flex pt-6 text-white mr-2 space-x-2">
           {speakerIcon} {wifiIcon} {chartBarIcon} {batteryIcon}
